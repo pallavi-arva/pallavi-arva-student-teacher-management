32ba5407bc66dd51eb8b527770e15e4eaa3fb9c8
ARVA PALLAVI <apallavi269@gmail.com>
2026-02-06 16:34:55 +0530

    Rename 'name' to 'courseName' in Course schema

--- backend/models/Course.js
+++ backend/models/Course.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
+
 const CourseSchema = new mongoose.Schema({
-  name: String,
+  courseName: { type: String, required: true },
+  // This links to the Teacher's unique ID in the database
   teacher: { type: mongoose.Schema.Types.ObjectId, ref: 'Teacher' },
+  // This is an array of Student IDs enrolled in the course
   students: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Student' }]
 });
+
 module.exports = mongoose.model('Course', CourseSchema);
